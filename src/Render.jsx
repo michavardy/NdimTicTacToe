1820f03411614b3489f72649cf0d262a7b805a29
micha <micha.vardy@tufin.com>
2022-11-07 08:21:37 +0200

    changes

--- src/Render.jsx
+++ src/Render.jsx
@@ -3,8 +3,19 @@ import { useState, createContext, useReducer, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import "./Render.css";
 
+
+//TODO 
+// write updatePlayers function that takes symbols and newNumPlayers and outputs players list
+// write in conseq Turns logic
+// format ctrl inputs nicely
+
 const initialState = {
     board:[[null, null, null],[null, null, null],[null, null, null]],
+    dimension:3,
+    symbols:["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+    numPlayers:2,
+    conseqTurnsPerSeq:1,
+    currTurnPerSeq:1,
     players:['x','o'],
     curPlayer:'x',
     turnNumber:0
@@ -38,6 +49,12 @@ export default function RenderGame() {
         <div className="ctrls">
             <button className="ctrl_button" onClick={() => dispatch({type:"reset",payload:{}})}>reset</button>
             <button className="ctrl_button" onClick={() => dispatch({type:"skip",payload:{}})}>skip</button>
+            <label className="ctrl_button">number of players</label>
+            <input className="ctrl_button" onChange={(e)=>dispatch({type:"updatePlayers",payload:{newNumPlayers:Number(e.nativeEvent.data)}})}></input>
+            <label className="ctrl_button">dimensions</label>
+            <input className="ctrl_button" onChange={(e)=>dispatch({type:"dimensions",payload:{dimension:Number(e.nativeEvent.data)}})}></input>
+            <label className="ctrl_button">consequitive moves per turn</label>
+            <input className="ctrl_button" onChange={(e)=>dispatch({type:"conseqTurns",payload:{conseqTurn:Number(e.nativeEvent.data)}})}></input>
         </div>
       </div>
   )};
@@ -58,18 +75,59 @@ function togglePlayer(curPlayer, playerList){
     }
 }
 
+function updatePlayers(newNumPlayers,state){
+    let newPlayerArray = [...Array(newNumPlayers).keys()].map(()=>{return state.symbols[Math.floor(Math.random()*state.symbols.length)]})
+    return newPlayerArray
+}
+
+function calcTurnPerSequence(state){
+    if (state.currTurnPerSeq  === state.conseqTurnsPerSeq){
+        let newTurnPerSeq = 1
+        let currPlayer = togglePlayer(state.curPlayer, state.players)
+        return [newTurnPerSeq,currPlayer]
+    }
+    else{
+        let newTurnPerSeq = state.currTurnPerSeq + 1
+        let currPlayer = state.curPlayer
+        return [newTurnPerSeq,currPlayer]
+    }
+    
+}
+
 function reducer (state,action){
     console.log(`reducer called: action: ${action.type}, payload: ${action.payload}`)
     switch(action.type){
         case "reset":
             return initialState;
         case "play":
+            [CurrTurn, CurrPlayer] = calcTurnPerSequence(state)
             return {...state, 
                 board:updateBoard(state.curPlayer, action.payload.position, state.board),
-                curPlayer:togglePlayer(state.curPlayer, state.players),
+                currTurnPerSeq:CurrTurn,
+                currPlayer:CurrPlayer,
                 turnNumber:state.turnNumber + 1}
         case "skip":
             return {...state, curPlayer:togglePlayer(state.curPlayer, state.players)}
+        case "dimensions":
+            return {...state,
+                board:[...Array(action.payload.dimension).keys()].map((j)=>{return ([...Array(action.payload.dimension).keys()].map((i)=>{return null}))}),
+                dimension:action.payload.dimension,
+                currPlayer:state.players[0],
+                turnNumber:0}
+        case "updatePlayers":
+            let newPlayers = updatePlayers(action.payload.newNumPlayers,state)
+            return {... state,
+                board: initialState.board,
+                numPlayers:action.payload.newNumPlayers,
+                players:newPlayers,
+                curPlayer:newPlayers[0],
+                turnNumber:0}
+        case "conseqTurns":
+            return {... state,
+                board: initialState.board,
+                conseqTurnsPerSeq:action.payload.conseqTurn,
+                currPlayer:state.players[0],
+                turnNumber:0}
         default:
             console.log(`reducer function called with action: ${action.type}`)
     }
